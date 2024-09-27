248970faf549a133caa982214adf64719a96db51
Công Nguyễn <93962331+nguyentiencong07032002@users.noreply.github.com>
2024-09-27 23:04:31 +0700

    Add files via upload

---
+++ b1/b1.js
@@ -0,0 +1,40 @@
+n = prompt("nhập tháng:");
+switch(n){
+    case "1":
+        console.log("tháng "+n+" có 31 ngày");
+        break;
+    case "2":
+        console.log("tháng "+n+" có 29 ngày");
+        break;
+    case "3":
+        console.log("tháng "+n+" có 31 ngày");
+        break;
+    case "4":
+        console.log("tháng "+n+" có 30 ngày");
+        break;
+    case "5":
+        console.log("tháng "+n+" có 31 ngày");
+        break;
+    case "6":
+        console.log("tháng "+n+" có 30 ngày");
+        break;
+    case "7":
+        console.log("tháng "+n+" có 31 ngày");
+        break;
+    case "8":
+        console.log("tháng "+n+" có 31 ngày");
+        break;
+    case "9":
+        console.log("tháng "+n+" có 30 ngày");
+        break;
+    case "10":
+        console.log("tháng "+n+" có 31 ngày");
+        break;
+    case "11":
+        console.log("tháng "+n+" có 30 ngày");
+        break;
+    case "12":
+        console.log("tháng "+n+" có 31 ngày");
+        break;   
+}
+
