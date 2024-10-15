e37928fb764f61a25b037f4db38b376af84053ba
Công Nguyễn <93962331+nguyentiencong07032002@users.noreply.github.com>
2024-10-15 21:10:00 +0700

    Add files via upload

--- Nguyễn Tiến Công/bai6/bai6.js
+++ Nguyễn Tiến Công/bai6/bai6.js
@@ -12,7 +12,7 @@ if (m < 1 || m > 12) {
     m === 12
   ) {
     let d = +prompt("nhập ngày");
-    if (d < 1 && d > 31) {
+    if (d < 1 || d > 31) {
       alert("ko hop le");
     } else {
       let year = +prompt("nhap nam");
@@ -22,7 +22,7 @@ if (m < 1 || m > 12) {
     let year = +prompt("nhap nam");
     if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
       let d = +prompt("nhập ngày");
-      if (d < 1 && d > 29) {
+      if (d < 1 || d > 29) {
         alert("ko hop le");
       } else {
         console.log("năm " + year + " có 366 ngày");
@@ -30,7 +30,7 @@ if (m < 1 || m > 12) {
       }
     } else {
       let d = +prompt("nhập ngày");
-      if (d < 1 && d > 28) {
+      if (d < 1 || d > 28) {
         alert("ko hop le");
       } else {
         console.log("năm " + year + " có 365 ngày");
@@ -39,7 +39,7 @@ if (m < 1 || m > 12) {
     }
   } else {
     let d = +prompt("nhập ngày");
-    if (d < 1 && d > 30) {
+    if (d < 1 || d > 30) {
       alert("ko hop le");
     } else {
       let year = +prompt("nhap nam");
