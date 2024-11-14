4be8c491553cd66b02741dffd3d1f3c4e1f686a3
Công Nguyễn <93962331+nguyentiencong07032002@users.noreply.github.com>
2024-11-14 11:56:57 +0700

    Add files via upload

--- nguyễn_tiến_công_12_11.sql
+++ nguyễn_tiến_công_12_11.sql
@@ -117,9 +117,6 @@ WHERE b.city= 'can tho' AND a.id=2;
 
 
 -- Hãy cho biết nơi tốt nghiệp của các kiến trúc sư (architect) đã thiết kế (design) công trình Khách Sạn Quốc Tế ở Cần Thơ (building)
-SELECT * FROM building;
-SELECT * FROM architect;
-
 SELECT a.`name` AS `kiến trúc sư`, a.place AS `nơi tốt nghiệp`, b.fname AS `công trình`
 FROM building b
 INNER JOIN design d ON d.building_id = b.id
@@ -127,9 +124,169 @@ INNER JOIN architect a ON d.architect_id = a.id
 WHERE b.fname = 'khach san quoc te' AND b.city = 'can tho';
 
 
+-- Cho biết họ tên, năm sinh, năm vào nghề của các công nhân có chuyên môn hàn hoặc điện (worker) đã tham gia các công trình (work) mà chủ thầu Lê Văn Sơn (contractor) đã trúng thầu (building)
+SELECT w.`name`AS `tên công nhân`, w.birthday AS `sinh nhật`, w.`year` AS `năm vào nghề`, w.skill AS `chuyên môn` 
+FROM worker w
+INNER JOIN `work` wo ON w.id = wo.worker_id
+INNER JOIN building b ON wo.building_id = b.id
+INNER JOIN contractor c ON c.id = b.contractor_id
+WHERE (w.skill = 'han' OR w.skill = 'dien') AND c.`name` = 'le van son';
+
+
+-- Những công nhân nào (worker) đã bắt đầu tham gia công trình Khách sạn Quốc Tế ở Cần Thơ (building) trong giai đoạn từ ngày 15/12/1994 đến 31/12/1994 (work) số ngày tương ứng là bao nhiêu
+SELECT w.`name`, wo.total AS `số ngày tương ứng`, b.fname, b.city
+FROM worker w
+INNER JOIN `work` wo ON wo.worker_id = w.id
+INNER JOIN building b ON b.id = wo.building_id
+WHERE (wo.`date` between '1994-12-15' and '1994-12-31')
+	and b.fname='khach san quoc te' and b.city='can tho';
+
+
+-- Cho biết họ tên và năm sinh của các kiến trúc sư đã tốt nghiệp ở TP Hồ Chí Minh (architect) và đã thiết kế ít nhất một công trình (design) có kinh phí đầu tư trên 400 triệu đồng (building)
+SELECT a.id, a.`name`, a.birthday
+FROM architect a
+JOIN design d ON d.architect_id = a.id
+JOIN building b ON b.id = d.building_id
+WHERE a.place = 'tp hcm' AND b.cost > 400;
+
+
+-- Cho biết tên công trình có kinh phí cao nhất
+SELECT max(cost) AS `công trình có kinh phí cao nhất`
+FROM building;
+
+
+-- Cho biết tên các kiến trúc sư (architect) vừa thiết kế các công trình (design) do Phòng dịch vụ sở xây dựng (contractor) thi công vừa thiết kế các công trình do chủ thầu Lê Văn Sơn thi công
+SELECT a.`name`AS `kiến trúc sư`, b.fname AS `công trình`, c.`name` AS `chủ thầu`
+FROM architect a
+JOIN design d ON d.architect_id = a.id
+JOIN building b ON b.id = d.building_id
+JOIN contractor c ON c.id = b.contractor_id
+WHERE c.`name` = 'phong dich vu so xd' AND c.`name` = 'le van son';  
+
+-- Cho biết họ tên các công nhân (worker) có tham gia (work) các công trình ở Cần Thơ (building) nhưng không có tham gia công trình ở Vĩnh Long
+SELECT wr.`name` AS `công nhân`, b.city AS `công trình`
+FROM worker wr
+JOIN `work` w ON w.worker_id = wr.id
+JOIN building b ON b.id = w.building_id
+WHERE b.city = 'can tho' AND b.city <> 'vinh long' ;
+
+-- Cho biết tên của các chủ thầu đã thi công các công trình có kinh phí lớn hơn tất cả các công trình do chủ thầu phòng Dịch vụ Sở xây dựng thi công
+SELECT c.`name`AS `chủ thầu` , b.`fname` AS `công trình` , b.cost AS `kinh phí`
+FROM contractor c
+JOIN building b ON c.id = b.contractor_id
+WHERE b.cost > all(
+	SELECT b.cost  
+	FROM building b
+	JOIN contractor c ON c.id = b.contractor_id
+	WHERE c.`name` = 'phong dich vu so xd'
+);
 
 
 
+-- Cho biết họ tên các kiến trúc sư có thù lao thiết kế một công trình nào đó dưới giá trị trung bình thù lao thiết kế cho một công trình
+SELECT a.`name` AS `kiến trúc sư`
+FROM architect a
+JOIN design d ON d.architect_id = a.id
+WHERE d.benefit < (
+	SELECT AVG(benefit) FROM design);
+ 
+
+-- Tìm tên và địa chỉ những chủ thầu đã trúng thầu công trình có kinh phí thấp nhất
+SELECT c.`name` AS `chủ thầu`, c.`address` AS `địa chỉ`, b.`fname` AS `công trình`
+FROM contractor c
+JOIN building b ON b.contractor_id = c.id
+WHERE b.cost = (
+		SELECT MIN(cost)
+        FROM building
+);
+
+-- Tìm họ tên và chuyên môn của các công nhân (worker) tham gia (work) các công trình do kiến trúc sư Le Thanh Tung thiet ke (architect) (design)
+SELECT wr.`name` AS `công nhân`, wr.`skill` AS `chuyên môn`, a.`name` AS `kiến trúc sư`, b.`fname` AS `công trình`
+FROM worker wr
+JOIN `work` w ON w.worker_id = wr.id
+JOIN building b ON b.id = w.building_id
+JOIN design d ON d.building_id = b.id
+JOIN architect a ON a.id = d.architect_id
+WHERE a.`name` = 'le thanh tung';
+
+
+-- Tìm các cặp tên của chủ thầu có trúng thầu các công trình tại cùng một thành phố
+SELECT b.id AS `id`, c.`name` AS `chủ thầu`, b.fname AS `công trình` 
+FROM contractor c
+JOIN building b ON b.contractor_id = c.id
+GROUP BY b.id;
+
+-- Tìm tổng kinh phí của tất cả các công trình theo từng chủ thầu
+SELECT c1.`name` AS `chủ thẩu 1`, c2.`name` AS `chủ thầu 2`,  b1.city
+FROM building b1
+JOIN building b2 ON b1.city = b2.city AND b1.id < b2.id
+JOIN contractor c1 ON b1.contractor_id = c1.id
+JOIN contractor c2 ON b2.contractor_id = c2.id
+WHERE b1.contractor_id <> b2.contractor_id;
+
+
+-- Tìm tổng kinh phí của tất cả các công trình theo từng chủ thầu
+SELECT c.id, c.`name`, SUM(cost) AS `tổng kinh phí các công trình`
+FROM building b
+JOIN contractor c ON c.id = b.contractor_id
+GROUP BY c.id;
+
+
+-- Cho biết họ tên các kiến trúc sư có tổng thù lao thiết kế các công trình lớn hơn 25 triệu
+SELECT a.id, a.`name`, SUM(d.benefit) 
+FROM architect a
+JOIN design d ON d.architect_id = a.id
+GROUP BY a.id, a.`name`
+HAVING SUM(d.benefit) >25;
+
+-- Cho biết số lượng các kiến trúc sư có tổng thù lao thiết kế các công trình lớn hơn 25 triệu
+SELECT COUNT(*) AS architects_count
+FROM (
+    SELECT a.id, SUM(d.benefit) AS total_benefit
+    FROM architect a
+    JOIN design d ON d.architect_id = a.id
+    GROUP BY a.id
+    HAVING total_benefit > 25
+) AS architect_with_benefit;
+
+
+-- Tìm tổng số công nhân đã than gia ở mỗi công trình
+SELECT b.id, COUNT(wr.id) AS `Tổng công nhân tham gia`
+FROM building b
+JOIN `work` w ON w.building_id = b.id
+JOIN worker wr ON wr.id = w.worker_id
+GROUP BY b.id;
+
+
+-- Tìm tên và địa chỉ công trình có tổng số công nhân tham gia nhiều nhất
+SELECT b.fname, b.address, COUNT(wr.id) AS total_workers
+FROM building b
+JOIN `work` w ON w.building_id = b.id
+JOIN worker wr ON wr.id = w.worker_id
+GROUP BY b.id
+HAVING total_workers = (
+	SELECT COUNT(wr.id) AS max_count
+	FROM building b
+	JOIN `work` w ON w.building_id = b.id
+	JOIN worker wr ON wr.id = w.worker_id
+	GROUP BY b.id
+	ORDER BY max_count DESC
+	limit 1
+);
+
+
+-- Cho biêt tên các thành phố và kinh phí trung bình cho mỗi công trình của từng thành phố tương ứng
+SELECT city, AVG(cost) AS `kinh phí TB`
+FROM building
+GROUP BY city;
+
+
+-- Cho biết họ tên các công nhân có tổng số ngày tham gia vào các công trình lớn hơn tổng số ngày tham gia của công nhân Nguyễn Hồng Vân
+-- Cho biết tổng số công trình mà mỗi chủ thầu đã thi công tại mỗi thành phố
+-- Cho biết họ tên công nhân có tham gia ở tất cả các công trình
+
+
+-- EX6
 
 
 
